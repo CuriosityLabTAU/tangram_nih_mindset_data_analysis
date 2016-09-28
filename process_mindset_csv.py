2ea70fdf46879751a179ef2003dea86191a2006c
Rinat <rinat.rosenberg.kima@gmail.com>
2016-09-28 08:48:44 +0300

    Rinat:  process_mindset_csv, process_spacial_csv

--- process_mindset_csv.py
+++ process_mindset_csv.py
@@ -5,75 +5,37 @@ import os
 # returns a list with [child selection, game result, number of moves, total time of game, child_selection,...]
 
 def analyze_result(filename, pathname='./processed_data/'):
+    b_start = False
+    #titles_sequence = ["selection_pre1","result_pre1","time_pre1",...]
+
+    correct_sequence = ["1_A","3_B","5_C","7_A","9_D","11_A","13_B","15_A","17_B","19_B","2!_C","23_D","25_D","27_D","29_B","31_C","2_D","4_C","6_D","8_A","10_C","12_D","14_C","16_C","18_A","20_D","22_A","24_B","26_C","28_B","30_A","32_B"]
+   # post_correct_sequence = ["2_D","4_C","6_D","8_A","10_C","12_D","14_C","16_C","18_A","20_D","22_A","24_B","26_C","28_B","30_A","32_B"]
+    result_list = []
 
     with open(os.path.join(pathname,filename), 'r') as fp:
+        i=0
         for line in fp:
             raw_dic = ast.literal_eval(line[6:])
             action = raw_dic['action']
             comment = raw_dic['comment']
             obj = raw_dic['obj']
             time = raw_dic['time']
+
+            if (b_start == False):
+                start_time = datetime.datetime.strptime(raw_dic['time'], '%Y_%m_%d_%H_%M_%S_%f')
+                b_start = True
             if (action == 'down'):
-                # comment=
-                #f_spatial_csv.write(action + "," + comment + "," + obj + "," + strDate + '\n')
-                print("obj=",obj)
-
-
-
-
-
-    child_play_flag = False
-    number_of_moves = 0
-    game_result = 0 # 0 for failure, 1 for success
-
-    result_list = []
-
-    with open(pathname + filename, 'r') as fp:
-        for line in fp:
-            dic = ast.literal_eval(line[6:])
-            if len(dic['comment'])>0:
-                # if dic['comment'][0] == 'select_treasure':
-                #     #print dic['comment']
-                #     #print dic['comment'][1][0]
-                if dic['comment'][0] == 'not_solved':
-                    # print dic['comment']
-                    #print dic['comment'][0]
-                    if child_play_flag is True:
-                        number_of_moves = number_of_moves + 1
-                if dic['comment'][0] == 'child_win':
-                    #print dic['comment'][0] + ' ' + dic['time']
-                    end_time = datetime.datetime.strptime(dic['time'], '%Y_%m_%d_%H_%M_%S_%f')
-                    total_time = end_time - start_time
-                    #print 'total time: '
-                    #print total_time
-                    game_result = 1 # child win
-                    result_list.append(game_result)
-                    result_list.append(number_of_moves)
-                    result_list.append(total_time.total_seconds())
-                    child_play_flag = False
-                    number_of_moves = 0
-                if dic['comment'][0] == 'finish':
-                    #print dic['comment'][0] + ' ' + dic['time']
-                    if child_play_flag is True:
-                        end_time = datetime.datetime.strptime(dic['time'], '%Y_%m_%d_%H_%M_%S_%f')
-                        total_time = end_time - start_time
-                        game_result = 0  # child lose
-                        result_list.append(game_result)
-                        result_list.append(number_of_moves)
-                        result_list.append(total_time.total_seconds())
-                        child_play_flag = False
-                        number_of_moves = 0
-                # if dic['comment'][0] == 'generate_selection':
-                #     print dic['comment'][0]
-                if dic['comment'][0] == 'press_treasure':
-                    if dic['comment'][3][0] == 'child_selection' and dic['comment'][2] == 'robot':
-                        #print 'child_selected ' + str(dic['comment'][1][0]) + ' ' + dic['time']
-                        child_play_flag = True
-                        start_time = datetime.datetime.strptime(dic['time'], '%Y_%m_%d_%H_%M_%S_%f')
-                        result_list.append(dic['comment'][1][0])
-
+                end_time = datetime.datetime.strptime(raw_dic['time'], '%Y_%m_%d_%H_%M_%S_%f')
+                total_time = end_time - start_time
+                start_time = end_time
+                result_list.append(obj)
+                if (obj == correct_sequence[i]):
+                    result_list.append(1)
+                else:
+                    result_list.append(0)
+                i = i + 1
+                result_list.append(total_time.total_seconds())
     return result_list
 
-
-#result = analyze_result('bag_tangram_17.txt', pathname='./processed_data/')
-#print result
+result = analyze_result('bag_mindset_test31.bag.txt', pathname='./processed_data/')
+print result
--- process_spatial_csv.py
+++ process_spatial_csv.py
@@ -2,11 +2,12 @@ import ast
 import datetime
 import os
 
-# returns a list with [child selection, game result, number of moves, total time of game, child_selection,...]
+# returns a list with [child selection, game result, total time of game, child_selection,...]
 
 def analyze_result(filename, pathname='./processed_data/'):
 
-    titles_sequence = ["pre_1","pre_2","pre_3","pre_4","pre_5","pre_6","pre_7","pre_8","pre_9","pre_10","pre_11","pre_12","pre_13","pre_14","pre_15","pre_16","post_1","post_2","post_3","post_4","post_5","post_6","post_7","post_8","post_9","post_10","post_11","post_12","post_13","post_14","post_15","post_16"]
+    #titles_sequence = ["selection_pre1","result_pre1","time_pre1",...]
+
     correct_sequence = ["1_A","3_B","5_C","7_A","9_D","11_A","13_B","15_A","17_B","19_B","2!_C","23_D","25_D","27_D","29_B","31_C","2_D","4_C","6_D","8_A","10_C","12_D","14_C","16_C","18_A","20_D","22_A","24_B","26_C","28_B","30_A","32_B"]
    # post_correct_sequence = ["2_D","4_C","6_D","8_A","10_C","12_D","14_C","16_C","18_A","20_D","22_A","24_B","26_C","28_B","30_A","32_B"]
     result_list = []
@@ -19,15 +20,21 @@ def analyze_result(filename, pathname='./processed_data/'):
             comment = raw_dic['comment']
             obj = raw_dic['obj']
             time = raw_dic['time']
+
+            if (comment == 'start'):
+                start_time = datetime.datetime.strptime(raw_dic['time'], '%Y_%m_%d_%H_%M_%S_%f')
             if (action == 'down'):
+                end_time = datetime.datetime.strptime(raw_dic['time'], '%Y_%m_%d_%H_%M_%S_%f')
+                total_time = end_time - start_time
+                start_time = end_time
                 result_list.append(obj)
                 if (obj == correct_sequence[i]):
                     result_list.append(1)
                 else:
                     result_list.append(0)
                 i = i + 1
-
+                result_list.append(total_time.total_seconds())
     return result_list
 
-#result = analyze_result('bag_spatial_test31.bag.txt', pathname='./processed_data/')
-#print result
+result = analyze_result('bag_spatial_test31.bag.txt', pathname='./processed_data/')
+print result
